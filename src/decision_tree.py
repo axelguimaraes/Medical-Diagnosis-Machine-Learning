3d2c37b9e2b8dc03bea8c749ef296ce5e33c82bc
Marcio Ribeiro <8200408@estg.ipp.pt>
2024-07-05 16:35:46 +0100

    Update decision_tree.py
    Map values into 0,1,2 depending on the value

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -3,29 +3,38 @@ from sklearn.tree import DecisionTreeClassifier, export_text
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
+
 def extract_decision_tree_rules(dataset_file):
     # Carregar o dataset
     df = pd.read_csv(dataset_file)
 
     # Preparar os dados
     le = LabelEncoder()
-    df['Fever'] = le.fit_transform(df['Fever'])
-    df['Cough'] = le.fit_transform(df['Cough'])
-    df['Fatigue'] = le.fit_transform(df['Fatigue'])
-    df['Difficulty Breathing'] = le.fit_transform(df['Difficulty Breathing'])
-    df['Gender'] = le.fit_transform(df['Gender'])
-    df['Blood Pressure'] = le.fit_transform(df['Blood Pressure'])
-    df['Cholesterol Level'] = le.fit_transform(df['Cholesterol Level'])
+    df['Fever'] = df['Fever'].map({'Yes': 1, 'No': 0})
+    df['Cough'] = df['Cough'].map({'Yes': 1, 'No': 0})
+    df['Fatigue'] = df['Fatigue'].map({'Yes': 1, 'No': 0})
+    df['Difficulty Breathing'] = df['Difficulty Breathing'].map(
+        {'Yes': 1, 'No': 0})
+    df['Gender'] = df['Gender'].map({'Male': 1, 'Female': 0})
+    df['Blood Pressure'] = df['Blood Pressure'].map(
+        {'Low': 0, 'Normal': 1, 'High': 2})
+    df['Cholesterol Level'] = df['Cholesterol Level'].map(
+        {'Low': 0, 'Normal': 1, 'High': 2})
 
     # Dividir em features (X) e target (y)
     X = df.drop(columns=['Disease', 'Outcome Variable'])  # Features
     y = df['Disease']  # Target
 
+   # print(df)
+    print(X)
+
     # Dividir o dataset em treino e teste
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
 
     # Treinar o modelo de Árvore de Decisão
-    model = DecisionTreeClassifier(random_state=42)
+    model = DecisionTreeClassifier(
+        random_state=42, max_depth=30, min_samples_split=2, min_samples_leaf=1)
     model.fit(X_train, y_train)
 
     # Extrair e interpretar as regras do modelo
@@ -34,12 +43,6 @@ def extract_decision_tree_rules(dataset_file):
 
     return tree_rules, X
 
-dataset_file_path = './data/dataset.csv'
-rules_text, X = extract_decision_tree_rules(dataset_file_path)
-
-# Print into a file the rules_text
-with open('./data/rules.txt', 'w') as f:
-    f.write(rules_text)
 
 def parse_rule(line):
     """Parse a single rule line and return a tuple (condition, operator, value)."""
@@ -55,10 +58,11 @@ def parse_rule(line):
     else:
         return None
 
+
 def remove_conflicting_conditions(conditions):
     cleaned_conditions = []
     conditions_dict = {}
-    
+
     for condition in conditions:
         feature, operator, value = condition
         if feature in conditions_dict:
@@ -70,11 +74,12 @@ def remove_conflicting_conditions(conditions):
                 conditions_dict[feature] = (operator, value)
         else:
             conditions_dict[feature] = (operator, value)
-    
+
     for feature, (operator, value) in conditions_dict.items():
         cleaned_conditions.append((feature, operator, value))
-    
+
     return cleaned_conditions
+
 
 def convert_to_prolog(input_file, output_file):
     with open(input_file, 'r') as file:
@@ -100,9 +105,12 @@ def convert_to_prolog(input_file, output_file):
                     conditions = remove_conflicting_conditions(conditions)
 
                     # Build the Prolog clause
-                    prolog_clause = f"disease(Age, Fever, Cough, CholesterolLevel, BloodPressure, DifficultyBreathing, \"{diagnosis}\") :-\n"
-                    condition_str = ',\n'.join([f"    {feature} {operator} {value}" for feature, operator, value in conditions])
-                    prolog_clause += condition_str.replace('|', '') + '.\n'  # Ensure '|' is removed'
+                    prolog_clause = f"disease(Age, Fever, Cough, CholesterolLevel, BloodPressure, DifficultyBreathing, \"{
+                        diagnosis}\") :-\n"
+                    condition_str = ',\n'.join([f"    {feature} {operator} {
+                                               value}" for feature, operator, value in conditions])
+                    # Ensure '|' is removed
+                    prolog_clause += condition_str.replace('|', '') + '.\n'
                     prolog_clauses.append(prolog_clause)
                     diagnosis = None
             else:
@@ -112,5 +120,13 @@ def convert_to_prolog(input_file, output_file):
         for clause in prolog_clauses:
             file.write(clause + '\n')
 
+
 # Use the function to convert the rules to Prolog format
+dataset_file_path = './data/dataset.csv'
+rules_text, X = extract_decision_tree_rules(dataset_file_path)
+
+# Print into a file the rules_text
+with open('./data/rules.txt', 'w') as f:
+    f.write(rules_text)
+
 convert_to_prolog('./data/rules.txt', './data/rules.pl')
