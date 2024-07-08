f7b1126a9ca9806079688c348a16b6a0cae65d10
Marcio Ribeiro <8200408@estg.ipp.pt>
2024-07-08 21:35:10 +0100

    Update unique_rules.pl

--- data/unique_rules.pl
+++ data/unique_rules.pl
@@ -1,18 +1,18 @@
-disease(_, Fever, _, _, _, _, DifficultyBreathing, "Negative") :- Fever =< 0.5, DifficultyBreathing > 0.5.
-disease(_, Fever, _, Gender, _, BloodPressure, DifficultyBreathing, "Negative") :- Fever > 0.5, BloodPressure =< 1.5, Gender > 0.5, DifficultyBreathing =< 0.5.
-disease(_, Fever, _, Gender, _, _, DifficultyBreathing, "Negative") :- Fever =< 0.5, DifficultyBreathing =< 0.5, Gender > 0.5.
-disease(_, Fever, _, _, _, BloodPressure, _, "Positive") :- Fever > 0.5, BloodPressure > 1.5.
-disease(_, Fever, _, Gender, _, BloodPressure, _, "Positive") :- Fever > 0.5, BloodPressure =< 1.5, Gender =< 0.5.
-disease(Age, Fever, _, Gender, _, _, _, "Positive") :- Fever =< 0.5, Age > 37.5, Gender > 0.5.
-disease(_, Fever, _, Gender, CholesterolLevel, _, DifficultyBreathing, "Negative") :- Fever > 0.5, CholesterolLevel =< 1.5, Gender > 0.5, DifficultyBreathing =< 0.5.
-disease(Age, Fever, _, _, _, _, _, "Negative") :- Fever =< 0.5, Age =< 39.0.
-disease(Age, Fever, _, _, _, _, _, "Positive") :- Fever =< 0.5, Age > 39.0.
-disease(_, Fever, _, Gender, CholesterolLevel, _, DifficultyBreathing, "Positive") :- Fever > 0.5, CholesterolLevel =< 1.5, Gender > 0.5, DifficultyBreathing > 0.5.
-disease(_, Fever, _, Gender, _, _, DifficultyBreathing, "Positive") :- Fever =< 0.5, DifficultyBreathing =< 0.5, Gender =< 0.5.
-disease(_, Fever, _, _, CholesterolLevel, _, DifficultyBreathing, "Positive") :- Fever > 0.5, CholesterolLevel =< 1.5, DifficultyBreathing > 0.5.
-disease(_, Fever, _, Gender, CholesterolLevel, _, _, "Positive") :- Fever > 0.5, CholesterolLevel =< 1.5, Gender =< 0.5.
-disease(_, Fever, _, _, CholesterolLevel, _, _, "Positive") :- Fever > 0.5, CholesterolLevel > 1.5.
-disease(_, Fever, _, _, CholesterolLevel, _, DifficultyBreathing, "Negative") :- Fever > 0.5, CholesterolLevel =< 1.5, DifficultyBreathing =< 0.5.
-disease(Age, Fever, _, Gender, _, _, _, "Positive") :- Fever =< 0.5, Age > 37.5, Gender =< 0.5.
-disease(_, Fever, _, Gender, _, BloodPressure, DifficultyBreathing, "Positive") :- Fever > 0.5, BloodPressure =< 1.5, Gender > 0.5, DifficultyBreathing > 0.5.
-disease(Age, Fever, _, _, _, _, _, "Negative") :- Fever =< 0.5, Age =< 37.5.
+assertz((disease(Age, Fever, _, Gender, _, _, _, 'Positive') :- Fever =< 0.5, Age > 37.5, Gender > 0.5)).
+assertz((disease(_, Fever, _, Gender, _, BloodPressure, DifficultyBreathing, 'Positive') :- Fever > 0.5, BloodPressure =< 1.5, Gender > 0.5, DifficultyBreathing > 0.5)).
+assertz((disease(_, Fever, _, Gender, _, _, DifficultyBreathing, 'Negative') :- Fever =< 0.5, DifficultyBreathing =< 0.5, Gender > 0.5)).
+assertz((disease(Age, Fever, _, _, _, _, _, 'Positive') :- Fever =< 0.5, Age > 39.0)).
+assertz((disease(Age, Fever, _, _, _, _, _, 'Negative') :- Fever =< 0.5, Age =< 39.0)).
+assertz((disease(Age, Fever, _, Gender, _, _, _, 'Positive') :- Fever =< 0.5, Age > 37.5, Gender =< 0.5)).
+assertz((disease(Age, Fever, _, _, _, _, _, 'Negative') :- Fever =< 0.5, Age =< 37.5)).
+assertz((disease(_, Fever, _, Gender, CholesterolLevel, _, DifficultyBreathing, 'Positive') :- Fever > 0.5, CholesterolLevel =< 1.5, Gender > 0.5, DifficultyBreathing > 0.5)).
+assertz((disease(_, Fever, _, _, CholesterolLevel, _, DifficultyBreathing, 'Negative') :- Fever > 0.5, CholesterolLevel =< 1.5, DifficultyBreathing =< 0.5)).
+assertz((disease(_, Fever, _, Gender, _, BloodPressure, _, 'Positive') :- Fever > 0.5, BloodPressure =< 1.5, Gender =< 0.5)).
+assertz((disease(_, Fever, _, _, _, _, DifficultyBreathing, 'Negative') :- Fever =< 0.5, DifficultyBreathing > 0.5)).
+assertz((disease(_, Fever, _, _, CholesterolLevel, _, DifficultyBreathing, 'Positive') :- Fever > 0.5, CholesterolLevel =< 1.5, DifficultyBreathing > 0.5)).
+assertz((disease(_, Fever, _, _, _, BloodPressure, _, 'Positive') :- Fever > 0.5, BloodPressure > 1.5)).
+assertz((disease(_, Fever, _, Gender, _, BloodPressure, DifficultyBreathing, 'Negative') :- Fever > 0.5, BloodPressure =< 1.5, Gender > 0.5, DifficultyBreathing =< 0.5)).
+assertz((disease(_, Fever, _, Gender, CholesterolLevel, _, _, 'Positive') :- Fever > 0.5, CholesterolLevel =< 1.5, Gender =< 0.5)).
+assertz((disease(_, Fever, _, _, CholesterolLevel, _, _, 'Positive') :- Fever > 0.5, CholesterolLevel > 1.5)).
+assertz((disease(_, Fever, _, Gender, _, _, DifficultyBreathing, 'Positive') :- Fever =< 0.5, DifficultyBreathing =< 0.5, Gender =< 0.5)).
+assertz((disease(_, Fever, _, Gender, CholesterolLevel, _, DifficultyBreathing, 'Negative') :- Fever > 0.5, CholesterolLevel =< 1.5, Gender > 0.5, DifficultyBreathing =< 0.5)).
